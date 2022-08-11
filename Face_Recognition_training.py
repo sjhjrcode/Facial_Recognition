e6f8e5316259e7102507536a6bcbad5014d78312
sjhjrcode <Steven.J.Howell.Code@gmail.com>
2022-08-11 11:49:40 -0500

    added label to labelmap;attempted face recognition

--- Face_Recognition_training.py
+++ Face_Recognition_training.py
@@ -13,6 +13,8 @@ import glob
 import time
 import face_recognition
 import cv2
+#gpus = tf.config.experimental.list_physical_devices('GPU')
+#tf.config.experimental.set_memory_growth(gpus[0], True)
 input_string = []
 
 while True:
@@ -321,3 +323,96 @@ plt.title('Training and Validation Loss')
 plt.xlabel('epoch')
 plt.show()
 '''
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.protos.string_int_label_map_pb2 import StringIntLabelMap, StringIntLabelMapItem
+from google.protobuf import text_format
+from tensorflow import keras
+#detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+
+detection_model = keras.models.load_model("./save/fine_tuning.h5")
+
+def convert_classes(classes, start=1):
+    msg = StringIntLabelMap()
+    for id, name in enumerate(classes, start=start):
+        msg.item.append(StringIntLabelMapItem(id=id, name=name))
+
+    text = str(text_format.MessageToBytes(msg, as_utf8=True), 'utf-8')
+    return text
+
+my_file = open("labels.txt", "r")
+
+data = my_file.read()
+data_into_list = data.split("\n")
+print(data_into_list)
+my_file.close()
+
+txt = convert_classes(data_into_list)
+print(txt)
+with open('label_map.pbtxt', 'w') as f:
+    f.write(txt)
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+
+
+
+category_index = label_map_util.create_category_index_from_labelmap('./label_map.pbtxt')
+
+
+#cap.release()
+
+
+
+# Setup capture
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while True: 
+    ret, frame = cap.read()
+    image_np = np.array(frame)
+    
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+    detections = detect_fn(input_tensor)
+    
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+                image_np_with_detections,
+                detections['detection_boxes'],
+                detections['detection_classes']+label_id_offset,
+                detections['detection_scores'],
+                category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=5,
+                min_score_thresh=.5,
+                agnostic_mode=False)
+
+    cv2.imshow('object detection',  cv2.resize(image_np_with_detections, (800, 600)))
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        break
+detections = detect_fn(input_tensor)
+
+
+#from matplotlib import pyplot as plt
+
+
+#tensorflow/core/kernels/data/generator_dataset_op.cc:108] Error occurred when finalizing GeneratorDataset 
